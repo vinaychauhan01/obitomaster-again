a2aa205dd6117d0ff72c6e8c54c23a8daf8fbfa1
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-12-31 19:56:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1650,7 +1650,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+            m=await message.reply_sticker(f"<b><i> CAACAgQAAxkBAAEB2AplkPZ8TzIIAAHIA8Za0pDweO2SfPEAAm4PAALh6rFQ2jEHmOgnEaIeBA'{search}'💥</i></b>")
             search = search.lower()
             find = search.split(" ")
             search = ""
@@ -1679,7 +1679,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+        m=await message.reply_sticker(f"<b><i> CAACAgQAAxkBAAEB2AplkPZ8TzIIAAHIA8Za0pDweO2SfPEAAm4PAALh6rFQ2jEHmOgnEaIeBA '{search}' 🔎</i></b>")
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
     pre = 'filep' if settings['file_secure'] else 'file'
