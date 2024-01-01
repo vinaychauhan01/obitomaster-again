1467452f115ec3b2999edd29f24317ee4b2958e1
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2024-01-01 15:46:45 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -4,7 +4,7 @@ import re
 import base64
 from pyrogram.file_id import FileId
 from pymongo.errors import DuplicateKeyError
-from pymongo import Instance, Document, fields
+from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
